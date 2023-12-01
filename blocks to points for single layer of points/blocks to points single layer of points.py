c185124db9a330028be7f98bc3801c4f3a431296
Amai <76209969+iamamai@users.noreply.github.com>
2023-12-01 17:48:54 +0800

    Add files via upload

--- blocks to points for single layer of points/blocks to points single layer of points.py
+++ blocks to points for single layer of points/blocks to points single layer of points.py
@@ -1,17 +1,37 @@
 import rhinoscriptsyntax as rs
 import random
 
+# Get a list of all layers in the Rhino document
 layers = rs.LayerNames()
+
+# Get a list of all block definitions in the Rhino document
 blocks = rs.BlockNames()
+
+# Prompt the user to select points in the Rhino viewport
 points = rs.GetObjects("Select points")
 
+# Prompt the user to set the degree interval for rotation
+deg = rs.GetInteger("Set degree interval (10, 30, 45, etc)")
 
+# Prompt the user to set the minimum scale factor as a decimal number
+scaleMin = rs.GetReal("Set a decimal number (0.5, 1.0, 2.5, etc)")
+
+# Prompt the user to set the maximum scale factor as a larger decimal number
+scaleMax = rs.GetReal("Set a larger decimal number")
+
+# Loop through each selected point
 for point in points:
-    ang = random.randrange(0, 366, 45)
-    rand = random.randrange(0, len(blocks))
-    rs.InsertBlock(blocks[rand], point, angle_degrees = ang) 
+    # Generate a random angle within the specified degree interval
+    ang = random.randrange(0, 366, deg)
 
-print(len(blocks))
-print(len(points))
-print(rand)
+    # Choose a random block from the available block definitions
+    rand_block = random.choice(blocks)
+
+    # Generate a random scale factor within the specified range
+    randscale = random.uniform(scaleMin, scaleMax)
+
+    # Insert the randomly chosen block at the selected point with random scale and rotation
+    rs.InsertBlock(rand_block, point, scale=(randscale, randscale, randscale), angle_degrees=ang)
+
+
 
